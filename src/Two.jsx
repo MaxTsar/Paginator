562e66130c068161194680dd6d2fd538f262605e
MaxTsar <rep0trett88@gmail.com>
2018-06-10 22:49:32 +0300

    sqush

--- src/Two.jsx
+++ src/Two.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
 
 export default class Two extends React.Component {
     state = {
@@ -12,8 +13,18 @@ export default class Two extends React.Component {
     }
 
     render() {
-        console.log(this.props.match.params.page)
         return (<section>
+                    <ul>
+                        <li>
+                            <NavLink to='/two/1'>{+this.props.match.params.page}</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/two/2'>{+this.props.match.params.page + 1}</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/two/3'>{+this.props.match.params.page + 2}</NavLink>
+                        </li>
+                    </ul>
                     <ul>
                         {
                             this.state.list.map((item, i) => <li key={i}>{i+1}</li>)
